fb1f07c5dc8c5dac1df5e64beef1e6333fb21d17
Dustin Lewis & Joshua Storey <student@epicodus.com>
2014-08-15 12:11:38 -0700

    Create save method to category class.

--- lib/category.rb
+++ lib/category.rb
@@ -3,7 +3,14 @@ class Category
 
   def initialize(attributes)
     @name = attributes[:name]
-    @id = attributes[:id]
+    @id = attributes[:id].to_i
   end
+
+  def save
+    results = DB.exec("INSERT INTO categories (name) VALUES ('#{name}') RETURNING id;")
+    @id = results.first['id'].to_i
+  end
+
+
 
 end
--- spec/category_spec.rb
+++ spec/category_spec.rb
@@ -6,13 +6,21 @@ describe Category do
   end
 
 
- describe "initialize" do
+ describe 'initialize' do
   it "intializes the category with a name" do
     expect(@test_category).to be_an_instance_of Category
   end
 
-  it "it returns the attributes of the current category" do
+  it "returns the attributes of the current category" do
     expect(@test_category.name).to eq 'Food'
   end
  end
+
+
+ describe 'save' do
+  it 'saves the category to the database and returns the id' do
+    expect(@test_category.save).to be_an_instance_of Fixnum
+    end
+  end
+
 end
