46ea8537e40da95402ec9e6856c823f6ade777a4
Dustin Lewis & Joshua Storey <student@epicodus.com>
2014-08-15 14:46:58 -0700

    Create method to save companies.

--- lib/company.rb
+++ lib/company.rb
@@ -1,9 +1,32 @@
 class Company
-  attr_reader :name
+  attr_reader :name, :id
 
   def initialize(attributes)
     @name = attributes[:name]
     @id = attributes[:id]
+  end
+
+  def save
+    results = DB.exec("INSERT INTO company (name) VALUES ('#{name}') RETURNING id;")
+    @id = results.first['id'].to_i
+  end
+
+  def self.all
+    companies =[]
+    results = DB.exec("SELECT * FROM company;")
+    results.each do |result|
+      attributes = {
+        :name => result['name'],
+        :id => result['id'].to_i,
+      }
+      current_company = Company.new(attributes)
+      companies << current_company
+    end
+    companies
+  end
+
+  def ==(another_company)
+    self.name == another_company.name && self.id == another_company.id
   end
 
 
--- spec/company_spec.rb
+++ spec/company_spec.rb
@@ -15,4 +15,30 @@ describe Company do
     expect(@test_company.name).to eq 'Wal-mart'
   end
  end
+
+  describe 'save' do
+  it 'saves the company to the database and returns the id' do
+    @test_company.save
+    expect(@test_company.id).to be_an_instance_of Fixnum
+    end
+  end
+
+  describe 'Self.all' do
+    it 'returns all of the saved categories' do
+      @test_company.save
+      expect(Company.all).to eq [@test_company]
+    end
+  end
+
+  describe '==(another_company)' do
+    it 'is true if two categories have the same attributes' do
+      @test_company.save
+      test_company = Company.new({:name => 'Wal-mart', :id => @test_company.id})
+      expect(test_company).to eq @test_company
+    end
+  end
+
+
+
+
 end
